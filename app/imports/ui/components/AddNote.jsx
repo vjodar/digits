03d70e1f4a60d51d4fb4c18efce233f4205b4bb4
vjodar <vjodar@hawaii.edu>
2019-11-09 23:15:18 -1000

    notes-1 finished in 40 minutes

---
+++ app/imports/ui/components/AddNote.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+import AutoForm from 'uniforms-semantic/AutoForm';
+import TextField from 'uniforms-semantic/TextField';
+import HiddenField from 'uniforms-semantic/HiddenField';
+import SubmitField from 'uniforms-semantic/SubmitField';
+import ErrorsField from 'uniforms-semantic/ErrorsField';
+import swal from 'sweetalert';
+import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
+import { PropTypes } from 'prop-types';
+import { Notes, NoteSchema } from '../../api/note/Note';
+
+/** Renders the Page for adding a document. */
+class AddNote extends React.Component {
+
+  /** On submit, insert the data. */
+  submit(data, formRef) {
+    const { note, owner, contactId, createdAt } = data;
+    Notes.insert({ note, owner, contactId, createdAt },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      });
+  }
+
+  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  render() {
+    let fRef = null;
+    return (
+        <AutoForm ref={ref => { fRef = ref; }} schema={NoteSchema} onSubmit={data => this.submit(data, fRef)} >
+          <Segment>
+            <TextField label="Add a timestamped note" name='note'/>
+            <SubmitField value='Submit'/>
+            <ErrorsField/>
+            <HiddenField name='owner' value={this.props.owner}/>
+            <HiddenField name='contactId' value={this.props.contactId}/>
+            <HiddenField name='createdAt' value={new Date()}/>
+          </Segment>
+        </AutoForm>
+    );
+  }
+}
+
+AddNote.propTypes = {
+  owner: PropTypes.string.isRequired,
+  contactId: PropTypes.string.isRequired,
+};
+
+export default AddNote;
--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Feed } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
+import AddNote from '../components/AddNote';
+import Note from '../components/Note';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Contact extends React.Component {
@@ -21,7 +23,15 @@ class Contact extends React.Component {
             </Card.Description>
           </Card.Content>
           <Card.Content extra>
+            <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
+          </Card.Content>
+          <Card.Content extra>
             <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
+          </Card.Content>
+          <Card.Content extra>
+            <Feed>
+              {this.props.notes.map((note, index) => <Note key={index} note={note}/>)}
+            </Feed>
           </Card.Content>
         </Card>
     );
@@ -31,6 +41,7 @@ class Contact extends React.Component {
 /** Require a document to be passed to this component. */
 Contact.propTypes = {
   contact: PropTypes.object.isRequired,
+  notes: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
